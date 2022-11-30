d1339630bf9054ed507e85a49a57349c9c109c93
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2022-11-30 14:13:24 +0900

    [UPDATE] #6 - 애니메이션 수정

--- design/js/menu.js
+++ design/js/menu.js
@@ -5,26 +5,36 @@ function slide(categoryDetail) {
 
     const height = list[0].offsetHeight;
     const totalHeight = height * list.length;
-    categoryDetail.style.height = totalHeight;
     
     const categoryListBox = document.querySelector(".category_list_box");
-    categoryListBox.style.height = (70 + totalHeight) + "px";
+    categoryListBox.style.height = (72 + totalHeight) + "px";
+    
 }
 
 function slideUp() {
     if (!mouseState) {
         const categoryListBox = document.querySelector(".category_list_box");
-        categoryListBox.style.height = 70 + "px";
+        categoryListBox.style.height = "72px";
     }    
 }
 
 function addEventDetail() {
-    const list = document.querySelectorAll(".category_list > li");
-    for (let idx = 0; idx < list.length; ++idx) {
-        list[idx].addEventListener("mouseover", function() {
+    // const list = document.querySelectorAll(".category_list > li");
+    // for (let idx = 0; idx < list.length; ++idx) {
+    //     list[idx].addEventListener("mouseover", function() {
+    //         mouseState = true;
+
+    //         slide(list[idx].querySelector(".category_detail"));
+    //     })        
+    // }
+
+    const boxes = document.querySelectorAll(".category_box");
+    for (let idx = 0; idx < boxes.length; ++idx) {
+        boxes[idx].addEventListener("mouseover", function() {
             mouseState = true;
 
-            slide(list[idx].querySelector(".category_detail"));
+            const detail = boxes[idx].parentNode.querySelector(".category_detail");
+            slide(detail);
         })        
     }
 
@@ -37,7 +47,7 @@ function addEventDetail() {
 
 
 function init() {
-    addEventDetail();
+    addEventDetail();    
 }
 
 init();
