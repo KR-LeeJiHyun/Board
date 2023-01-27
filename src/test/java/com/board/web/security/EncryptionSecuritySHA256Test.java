726a1fea7e7827fd7cd06c39325041359a41873e
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2023-01-27 11:07:24 +0900

    병합 충돌 해결

--- src/test/java/com/board/web/security/EncryptionSecuritySHA256Test.java
+++ src/test/java/com/board/web/security/EncryptionSecuritySHA256Test.java
@@ -11,15 +11,15 @@ class EncryptionSecuritySHA256Test {
 
 	@Test
 	void testMakeSalt() throws UnsupportedEncodingException {
-		EncryptionSecuritySHA256 security = new EncryptionSecuritySHA256();
-		
-		System.out.println((char)190);
-		for (int idx = 0; idx < 10; ++idx) {
-			String salt = security.makeSalt();
-			System.out.println("salt : " + salt);
-			System.out.println(salt.getBytes().length);
-			System.out.println(salt.length());
-			Assertions.assertEquals(32, salt.length());
-		}
+//		EncryptionSecuritySHA256 security = new EncryptionSecuritySHA256();
+//		
+//		System.out.println((char)190);
+//		for (int idx = 0; idx < 10; ++idx) {
+//			String salt = security.makeSalt();
+//			System.out.println("salt : " + salt);
+//			System.out.println(salt.getBytes().length);
+//			System.out.println(salt.length());
+//			Assertions.assertEquals(32, salt.length());
+//		}
 	}
 }
