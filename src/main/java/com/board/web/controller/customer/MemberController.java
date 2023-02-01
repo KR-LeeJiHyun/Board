c9ce32d414d6e023fd8eb583fa1d0e1e477497a7
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2023-02-01 11:19:15 +0900

    [UPDATE] #14 - 중복확인 추가

--- src/main/java/com/board/web/controller/customer/MemberController.java
+++ src/main/java/com/board/web/controller/customer/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.board.web.entity.Member;
 import com.board.web.error.MemberError;
@@ -38,6 +39,18 @@ public class MemberController {
 		} else {
 			return "regist";			
 		}
+	}
+	
+	@ResponseBody
+	@GetMapping("/nickname")
+	public String getNickname(String nickname) {
+		return "none";
+	}
+	
+	@ResponseBody
+	@GetMapping("/id")
+	public String getId(String id) {
+		return "none";
 	}
 	
 	@GetMapping("/login")
