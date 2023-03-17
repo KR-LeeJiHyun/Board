b265bcfd9ed8e79d9c78d4e402e2bbef8ad2148c
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2023-03-17 14:44:18 +0900

    [UPDATE] #22 - 인터셉터 설정 방식 변경

--- src/main/java/com/board/web/config/AppConfig.java
+++ src/main/java/com/board/web/config/AppConfig.java
@@ -3,11 +3,21 @@ package com.board.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+
+import com.board.web.interceptor.LoginCheckInterceptor;
 
 @Configuration
 public class AppConfig {
 	@Bean
 	public Pbkdf2PasswordEncoder pbkdf2PasswordEncoder() {
 		return new Pbkdf2PasswordEncoder();
+	} 
+	
+	@Bean
+	public MappedInterceptor loginCheckInterceptor() {
+		String[] includePatterns = {"/**"};
+		String[] excludePatterns = {"/members/**","/css/**","/js/**","/fonts/**","/images/**"};
+	    return new MappedInterceptor(includePatterns, excludePatterns, new LoginCheckInterceptor());
 	}
 }
