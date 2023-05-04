dfaeb4444b89b5b317bda5581ab8d10703a0449b
wl9322 <wl9322@gmail.com>
2023-05-04 13:16:40 +0900

    [UPDATE] #22 - 멤버 테이블에 매핑되는 정보 추가

--- src/main/java/com/board/web/entity/Member.java
+++ src/main/java/com/board/web/entity/Member.java
@@ -8,6 +8,7 @@ public class Member {
 	private String id;
 	private String email;
 	private Date birthday;
+	private String encryptedPassword;
 	
 	public Member() {
 		this.name = null;
@@ -17,52 +18,37 @@ public class Member {
 		this.birthday = null;
 	}
 	
-	public Member(String name, String nickname, String id, String email, Date birthday) {
+	public Member(String name, String nickname, String id, String email, Date birthday, String encryptedPassword) {
 		this.name = name;
 		this.nickname = nickname;
 		this.id = id;
 		this.email = email;
 		this.birthday = birthday;
+		this.encryptedPassword = encryptedPassword;
 	}
 
 	public String getName() {
 		return name;
 	}
 	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 	public String getNickname() {
 		return nickname;
 	}
 	
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
 	
 	public String getId() {
 		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
 	}
 	
 	public String getEmail() {
 		return email;
 	}
 	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
 	public Date getBirthday() {
 		return birthday;
 	}
 	
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
+	public String getEncryptedPassword() {
+		return encryptedPassword;
 	}
-	
 }
