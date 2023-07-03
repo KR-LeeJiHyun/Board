9572b5cf55d7b30cc7a07dff2e9a20c7db56da3d
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2023-07-03 15:38:16 +0900

    [UPDATE] #31 - 댓글 기능 정의 추가 및 변경

--- src/main/java/com/board/web/repository/CommentRepository.java
+++ src/main/java/com/board/web/repository/CommentRepository.java
@@ -5,12 +5,15 @@ import java.util.List;
 import com.board.web.controller.comment.CommentForm;
 import com.board.web.entity.Comment;
 import com.board.web.entity.Member;
+import com.board.web.repository.jdbc.comment.CommentDTO;
 
 public interface CommentRepository {
-	int insertComment(Member member, CommentForm commentForm);
+	long insertComment(Member member, CommentForm commentForm);
 	Comment findOne(String category, Long commentId);
-	List<Comment> findComments(String category, Long postId, int page);
+	List<CommentDTO> findComments(String category, Long postId, int page);
+	List<CommentDTO> findReplyComments(String category, Long postId, Long parentId);
 	Long findTotalCount(String category, Long postId);
+	Long findTopTotalCount(String category, Long postId);
 	int findCount(String category, String column, Long commentId);
 	int updateCount(String category, String column, Long commentId);
 	int updateContent(String category, Long commentId, String content);
